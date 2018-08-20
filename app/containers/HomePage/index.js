6b02535ae961b5ab1a705035760156e50bb22c04
Dito <dito@lizard-apps.com>
2018-08-20 18:34:01 +0800

    add name

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -10,16 +10,41 @@
  */
 
 import React from 'react';
-import { FormattedMessage } from 'react-intl';
-import messages from './messages';
+import styled from 'styled-components';
+const Div = styled.div`
+  margin: auto;
+  padding: 10%;
+`;
+const Box = styled.div`
+  margin: 0 10%;
+  width: 80%;
+  text-align: center;
+`;
+const H1 = styled.h1`
+  color: #222;
+  font-size: 36px;
+  font-family: 'Lato';
+  font-weight: 300;
+  display: inline;
+`;
+const H1Bold = H1.extend`
+  font-weight: 600;
+`;
 
 /* eslint-disable react/prefer-stateless-function */
 export default class HomePage extends React.PureComponent {
   render() {
     return (
-      <h1>
-        <FormattedMessage {...messages.header} />
-      </h1>
+      <Div>
+        <Box>
+          <H1>I'm </H1>
+          <H1Bold>Dito Anwar Baskoro</H1Bold>
+          <H1>, a Software Engineer </H1><br/>
+          <H1>From Jakarta Indonesia </H1><br/>
+          <H1>Currently living and working in </H1><br/>
+          <H1>Kuala Lumpur Malaysia</H1>
+        </Box>
+      </Div>
     );
   }
 }
